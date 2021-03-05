07ff955f67a561b0087eb3309a7daade047e0326
Peter Burnett <peterburnett@catalyst-au.net>
2021-03-05 14:54:31 +1000

    MDL-71031 webservices: Extended gradecategory creation API
    This adds a new webservice that creates gradecategories as a batch,
    and deprecates the old single creation call, which is superseded.
    It also patches a few small issues in the single WS, for any integrations
    currently relying on that webservice.

--- lib/tests/grades_externallib_test.php
+++ lib/tests/grades_externallib_test.php
@@ -539,25 +539,26 @@ class core_grades_external_testcase extends externallib_advanced_testcase {
      *
      * @return void
      */
-    public function test_create_gradecategory() {
+    public function test_create_gradecategories() {
         global $DB;
         $this->resetAfterTest(true);
         $course = $this->getDataGenerator()->create_course();
         $this->setAdminUser();
 
         // Test the most basic gradecategory creation.
-        $status1 = core_grades_external::create_gradecategory($course->id, 'Test Category 1', []);
+        $status1 = core_grades_external::create_gradecategories($course->id, [['fullname' => 'Test Category 1', 'options' => []]]);
 
-        $courseparentcat = new grade_category(['courseid' => $course->id, 'depth' => 1], true);
-        $record1 = $DB->get_record('grade_categories', ['id' => $status1['categoryid']]);
+        $courseparentcat = grade_category::fetch_course_category($course->id);
+        $record1 = $DB->get_record('grade_categories', ['id' => $status1['categoryids'][0]]);
         $this->assertEquals('Test Category 1', $record1->fullname);
         // Confirm that the parent category for this category is the top level category for the course.
         $this->assertEquals($courseparentcat->id, $record1->parent);
         $this->assertEquals(2, $record1->depth);
 
         // Now create a category as a child of the newly created category.
-        $status2 = core_grades_external::create_gradecategory($course->id, 'Test Category 2', ['parentcategoryid' => $record1->id]);
-        $record2 = $DB->get_record('grade_categories', ['id' => $status2['categoryid']]);
+        $status2 = core_grades_external::create_gradecategories($course->id,
+            [['fullname' => 'Test Category 2', 'options' => ['parentcategoryid' => $record1->id]]]);
+        $record2 = $DB->get_record('grade_categories', ['id' => $status2['categoryids'][0]]);
         $this->assertEquals($record1->id, $record2->parent);
         $this->assertEquals(3, $record2->depth);
         // Check the path is correct.
@@ -586,8 +587,9 @@ class core_grades_external_testcase extends externallib_advanced_testcase {
             'parentcategoryid' => $record2->id
         ];
 
-        $status3 = core_grades_external::create_gradecategory($course->id, 'Test Category 3', $customopts);
-        $cat3 = new grade_category(['courseid' => $course->id, 'id' => $status3['categoryid']], true);
+        $status3 = core_grades_external::create_gradecategories($course->id,
+            [['fullname' => 'Test Category 3', 'options' => $customopts]]);
+        $cat3 = new grade_category(['courseid' => $course->id, 'id' => $status3['categoryids'][0]], true);
         $cat3->load_grade_item();
 
         // Lets check all of the data is in the right shape.
@@ -610,6 +612,36 @@ class core_grades_external_testcase extends externallib_advanced_testcase {
         // Coefficient is converted to percentage.
         $this->assertEquals(0.2, $cat3->grade_item->aggregationcoef2);
         $this->assertEquals($record2->id, $cat3->parent);
-    }
 
+
+        // Now test creating 2 in parallel, and nesting them.
+        $status4 = core_grades_external::create_gradecategories($course->id, [
+            [
+                'fullname' => 'Test Category 4',
+                'options' => [
+                    'idnumber' => 'secondlevel'
+                ],
+            ],
+            [
+                'fullname' => 'Test Category 5',
+                'options' => [
+                    'idnumber' => 'thirdlevel',
+                    'parentcategoryidnumber' => 'secondlevel'
+                ],
+            ],
+        ]);
+
+        $secondlevel = $DB->get_record('grade_categories', ['id' => $status4['categoryids'][0]]);
+        $thirdlevel = $DB->get_record('grade_categories', ['id' => $status4['categoryids'][1]]);
+
+        // Confirm that the parent category for secondlevel is the top level category for the course.
+        $this->assertEquals($courseparentcat->id, $secondlevel->parent);
+        $this->assertEquals(2, $record1->depth);
+
+        // Confirm that the parent category for thirdlevel is the secondlevel category.
+        $this->assertEquals($secondlevel->id, $thirdlevel->parent);
+        $this->assertEquals(3, $thirdlevel->depth);
+        // Check the path is correct.
+        $this->assertEquals('/' . implode('/', [$courseparentcat->id, $secondlevel->id, $thirdlevel->id]) . '/', $thirdlevel->path);
+    }
 }
